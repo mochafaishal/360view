5b46a0f7f525ac8070268953cfd9fd169adc85bc
mochafaishal <mochammadfaishal21@gmail.com>
2020-06-11 20:09:12 +0700

    a

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <h1>INI CONTOH NYA :</h1> <br>
 
-<iframe width='853' height='480' src='https://my.matterport.com/show/?m=sAKHVseb88f' frameborder='0' allowfullscreen allow='xr-spatial-tracking'></iframe>
 
